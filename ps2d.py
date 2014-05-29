8ca048bfb8da7eb7d3843b44a0249323795d6444
RyanAntony <ryanstevens@ryans-air.home>
2014-05-29 00:42:26 -0400

    hangman

---
+++ ps2d.py
@@ -0,0 +1,62 @@
+# 6.00 Problem Set 3
+# 
+# Hangman
+#
+
+
+# -----------------------------------
+# Helper code
+# (you don't need to understand this helper code)
+import random
+import string
+
+WORDLIST_FILENAME = "words.txt"
+
+def load_words():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+    
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print "Loading word list from file..."
+    # inFile: file
+    inFile = open(WORDLIST_FILENAME, 'r', 0)
+    # line: string
+    line = inFile.readline()
+    # wordlist: list of strings
+    wordlist = string.split(line)
+    print "  ", len(wordlist), "words loaded."
+    return wordlist
+
+def choose_word(wordlist):
+    """
+    wordlist (list): list of words (strings)
+
+    Returns a word from wordlist at random
+    """
+    return random.choice(wordlist)
+
+# end of helper code
+# -----------------------------------
+
+# actually load the dictionary of words and point to it with 
+# the wordlist variable so that it can be accessed from anywhere
+# in the program
+wordlist = load_words()
+
+# your code begins here!
+
+def hangman():
+    """This is a one player game, vs the computer."""
+    print "Welcome to HANGMAN!"
+    word = choose_word(wordlist)
+    letters_remain = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    print "The word I am thinking of has",len(word),"letters."
+    for i in 
+    place_holder = len(word)*"_ "
+    print place_holder
+    guess = raw_input('Give me a letter: ')
+    for i in letters_remain:
+        if i == guess:
+            
